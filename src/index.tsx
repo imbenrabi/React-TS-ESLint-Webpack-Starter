64a93c4312b69f2a70039acd033db5c611591220
Ben Rabi <benr@wix.com>
2022-05-28 19:36:24 +0300

    fix(): opening sentence

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const App = () => (
-  <h1>Welcome to the weakest challenge{" "}{new Date().toLocaleDateString()}</h1>
+  <h1>A new day! {" "}{new Date().toLocaleDateString()}</h1>
 );
 
 ReactDOM.render(
